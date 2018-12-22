dbcc8e949df39e5e412f3b29d5dc26aa7bb8db64
maxim134vlg <tatarinmaxx@gmail.com>
2018-12-22 23:54:40 +0400

    Registration add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#mainpage'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
